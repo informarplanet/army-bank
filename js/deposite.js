da77a8605b85333f6b85be05edb358b1ef755989
Dead Army <pulakp18@gmail.com>
2024-05-05 12:59:24 +0530

    update

--- js/deposite.js
+++ js/deposite.js
@@ -18,12 +18,7 @@ document.getElementById('depoBtn').addEventListener('click', function(){
     // update clear balance
     const newClearbalance=depositeAmount+previousClearbalance;
     insertValue('clearBlance',newClearbalance);
-<<<<<<< HEAD
     // make log
     insertLog(depositeAmount,'deposite',newClearbalance)
 
 })
-=======
-
-})
->>>>>>> 11994737502c8697c082a70bb67e205c940b970c
