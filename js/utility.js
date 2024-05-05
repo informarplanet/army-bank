435f7b31e4093105d778e557495fc3f22c092e65
DeadArmy <84080385+informarplanet@users.noreply.github.com>
2024-05-05 08:29:13 +0530

    Update utility.js

--- js/utility.js
+++ js/utility.js
@@ -5,6 +5,16 @@ function getvaluebyLeble (amountValue){
         getAmount.value="";
         return finalamount;
 }
+// checking valid amount
+function validData(value){
+    if(isNaN(value)){
+        alert('Please input number');
+    }else if(value>10000){
+        alert('you don"t deposite / widthdraw upto 10000');
+    }else{
+        return value;
+    }
+}
 
 function getvaluebyInnertext(amountText){
     const gettext=document.getElementById(amountText);
